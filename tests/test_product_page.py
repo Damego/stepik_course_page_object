a530d4e08ff21e2f0c1325ce48387250089670c6
Damego <damego.dev@gmail.com>
2025-06-08 13:18:41 +0500

    mark tests; add readme

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -6,6 +6,7 @@ from ..pages import ProductPage, BasketPage
 PRODUCT_PAGE_URL = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize(
     'link', 
     [
@@ -60,8 +61,9 @@ def test_guest_should_see_login_link_on_product_page(browser: WebDriver):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
-    
+
+
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -70,6 +72,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser: WebDriver):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
