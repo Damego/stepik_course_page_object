1d1a670cdcc83d4ffa4650d07ed34270685c42b6
Damego <damego.dev@gmail.com>
2025-06-08 11:31:24 +0500

    add tests for login page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from ..pages import MainPage
+from ..pages import MainPage, LoginPage
 
 MAIN_PAGE_URL = "http://selenium1py.pythonanywhere.com/"
 
@@ -10,3 +10,8 @@ def test_guest_can_go_to_login_page(browser: WebDriver):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+    
